c20a994b8abf2768cdc02d1fa051f5bebfa6b4fb
Frolenko Dima <mail@frolenkodima.ru>
2023-08-31 07:46:50 +0300

    добавить функцию получения токена доступа из GraphQL - NativeRBdata

--- rbdata/native_api.py
+++ rbdata/native_api.py
@@ -11,4 +11,34 @@ class NativeRbData:
         self.__email=email
         self.__password=password
 
+        
+    def get_token(self)->str:
+        graphql_query="""
+            mutation {{
+                session {{
+                    createWithPassword(
+                            input:{{
+                                email:"{email}",
+                                password:"{password}"}})
+                        {{
+                        sessionTokens{{
+                            accessToken
+                        }}
+                    }}
+                }}
+            }}
+            """.format(email=self.__email,password=self.__password)
+ 
+        request_body={"query":graphql_query}
+        resp=requests.post(self.api_url,json=request_body)
+        if not resp.ok:
+            print("Cant get token from graphql")
+            return None
+        
+        try:
+            token=resp.json()['data']['session']['createWithPassword']['sessionTokens']['accessToken']
+            return token
+        except Exception:
+            print("cant parse answer from graphql, when trying to get token")
+            return None
 
