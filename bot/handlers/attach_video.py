0d32ca340327aa36afe7eada3bda9f7ba2515e15
Frolenko Dima <mail@frolenkodima.ru>
2023-09-01 10:51:49 +0300

    fix: bot.handlers.append_video Поменять название хендлера на более логичное

--- bot/handlers/attach_video.py
+++ bot/handlers/attach_video.py
@@ -27,7 +27,7 @@ def extract_video_id(text, messaging_callback):
 # Handlers
 
 @bot.message_handler(func=replies_to(from_bot,has_link_to_storage, is_numeric))
-def append_match_id(message:Message):
+def on_numeric_reply_to_links(message:Message):
     """Привязывает видео к матчу, если ссылка на матч только одна и получино сообщение из цифр, оно отправлено в ответ на сообщение со ссылкой на видео"""
     message_with_video=message.reply_to_message
     video_id=extract_video_id(message_with_video.text,messaging_callback=lambda text: bot.reply_to(message,text=text))
