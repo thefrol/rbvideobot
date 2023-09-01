e565244bc085b8244a7ba3c211a1029028080ec2
Frolenko Dima <mail@frolenkodima.ru>
2023-09-01 10:23:44 +0300

    bot.handlers Успрощен хендлер для привязки видео.
    реагирует только на сообщения содержащие только цифры

--- bot/handlers/attach_video.py
+++ bot/handlers/attach_video.py
@@ -6,7 +6,7 @@ import re
 
 from ..bot import bot
 import settings
-from .conditionals import is_a_reply, reply_to_message, from_bot,has_link_to_storage, and_
+from .conditionals import is_a_reply, is_numeric, reply_to_message, from_bot,has_link_to_storage, and_
 
 # Handlers
 
@@ -23,23 +23,18 @@ def extract_video_id(text, messaging_callback):
         video_id=m[0].group("video_id")
         return video_id
 
-@bot.message_handler(func=reply_to_message(and_(from_bot,has_link_to_storage)))
+@bot.message_handler(func=reply_to_message(and_(from_bot,has_link_to_storage, is_numeric)))
 def append_match_id(message:Message):
     """Привязывает видео к матчу, если ссылка на матч только одна и получино сообщение из цифр, оно отправлено в ответ на сообщение со ссылкой на видео"""
-    if message.text.isnumeric:
-        message_with_video=message.reply_to_message
-        video_id=extract_video_id(message_with_video.text,messaging_callback=lambda text: bot.reply_to(message,text=text))
-        match_id=message.text
-        #TODO проверка, а существует ли такое видео вообще!
+    message_with_video=message.reply_to_message
+    video_id=extract_video_id(message_with_video.text,messaging_callback=lambda text: bot.reply_to(message,text=text))
+    match_id=message.text
+    #TODO проверка, а существует ли такое видео вообще!
 
-        n=NativeRbData(os.getenv("RBDATA_EMAIL"),os.getenv("RBDATA_PASSWORD"))
-        if(n.attach_match(match_id=match_id, video_id=video_id)):
-            bot.send_message(chat_id=message.chat.id,text="🙌")
-            bot.send_message(chat_id=message.chat.id,text=f"Привязал `видео {video_id}` к `матчу {match_id}`",parse_mode="MARKDOWN") #TODO писать имя видео и матча! или ссылки хотябы            
-        else:
-            bot.send_message(chat_id=message.chat.id,text="😢")
-            bot.send_message(chat_id=message.chat.id,text=f"Я не смог привязать `видео {video_id}` к `матчу {match_id}`",parse_mode="MARKDOWN") 
-
-        return 
-    
-    bot.reply_to(message=message,text="debug(handlers.append_match)")
+    n=NativeRbData(os.getenv("RBDATA_EMAIL"),os.getenv("RBDATA_PASSWORD"))
+    if(n.attach_match(match_id=match_id, video_id=video_id)):
+        bot.send_message(chat_id=message.chat.id,text="🙌")
+        bot.send_message(chat_id=message.chat.id,text=f"Привязал `видео {video_id}` к `матчу {match_id}`",parse_mode="MARKDOWN") #TODO писать имя видео и матча! или ссылки хотябы            
+    else:
+        bot.send_message(chat_id=message.chat.id,text="😢")
+        bot.send_message(chat_id=message.chat.id,text=f"Я не смог привязать `видео {video_id}` к `матчу {match_id}`",parse_mode="MARKDOWN") 
