2ba52ab10aaf29934929e37549f5f5f0d6cc2fd7
Frolenko Dima <mail@frolenkodima.ru>
2023-09-01 11:32:03 +0300

    feature: bot.handlers.attach_video добавить парсинг сообщений с параметрами вида a=123 b:1233 один параметр на сообщение

--- bot/handlers/attach_video.py
+++ bot/handlers/attach_video.py
@@ -8,8 +8,10 @@ from ..bot import bot
 import settings
 from .conditionals import is_a_reply, is_numeric, replies_to, from_bot,has_link_to_storage, and_
 
+SEPARATORS=':='
+#TODO переименовать бы в update_video или типа того
 
-# Handlers
+# Helpers
 
 def extract_video_id(text, messaging_callback):
 
@@ -24,6 +26,17 @@ def extract_video_id(text, messaging_callback):
         video_id=m[0].group("video_id")
         return video_id
 
+def extract_parameter(text:str)-> (str,str,bool):
+    """Достает параметр из cообщения типа `id=2`
+    возвращает: parameter_name str, parameter_value str, success bool
+    вернет ("id","2", True) или 
+    None, None, False — в случае ошибки в последнем параметре(success) передается False"""
+    pattern=rf'(?P<name>\w+)[{SEPARATORS}](?P<value>.*)'
+    m=re.search(pattern=pattern,string=text)
+    if m is None:
+        return None,None,False
+    return m.group("name"),m.group("value"), True
+
 def append_match_id(match_id,video_id, messaging_callback):
     n=NativeRbData(os.getenv("RBDATA_EMAIL"),os.getenv("RBDATA_PASSWORD"))
     if(n.attach_match(match_id=match_id, video_id=video_id)):
@@ -34,6 +47,14 @@ def append_match_id(match_id,video_id, messaging_callback):
         messaging_callback(text=f"Я не смог привязать `видео {video_id}` к `матчу {match_id}`",parse_mode="MARKDOWN") 
 
 
+# Conditionals
+
+def is_parameter_setting(message:Message):
+    """определяет если текст сообщения используется для натройки параметров, то есть имеет вид
+    `name=Видео` или `matchid: 123`"""
+
+    return any((sep in message.text for sep in SEPARATORS))
+     
 
 # Handlers
 
@@ -49,11 +70,29 @@ def on_numeric_reply_to_links(message:Message):
     #TODO проверка, а существует ли такое видео вообще!
     append_match_id(match_id=match_id, video_id=video_id,messaging_callback=write_to_chat_callback)
 
+# связь параметров и функций, которые они вызывают
+func_map={
+    ("matchId","match","matchid","match_id"): lambda video_id, value, messaging_callback: append_match_id(video_id=video_id, match_id=value,messaging_callback=messaging_callback),
+    ("name","videoName","video_name"): lambda video_id, value, messaging_callback: messaging_callback("Not Implemented")
+}
 
-    n=NativeRbData(os.getenv("RBDATA_EMAIL"),os.getenv("RBDATA_PASSWORD"))
-    if(n.attach_match(match_id=match_id, video_id=video_id)):
-        bot.send_message(chat_id=message.chat.id,text="🙌")
-        bot.send_message(chat_id=message.chat.id,text=f"Привязал `видео {video_id}` к `матчу {match_id}`",parse_mode="MARKDOWN") #TODO писать имя видео и матча! или ссылки хотябы            
-    else:
-        bot.send_message(chat_id=message.chat.id,text="😢")
-        bot.send_message(chat_id=message.chat.id,text=f"Я не смог привязать `видео {video_id}` к `матчу {match_id}`",parse_mode="MARKDOWN") 
+@bot.message_handler(
+    func=and_(is_parameter_setting, replies_to(from_bot,has_link_to_storage)))
+def on_parametric_message_in_reply_to_link(message:Message):
+    reply_to_callback=lambda text,**kwargs: bot.reply_to(message,text=text,**kwargs)
+    write_to_chat_callback=lambda text,**kwargs: bot.send_message(chat_id=message.chat.id,text=text,**kwargs)
+    message_with_video=message.reply_to_message
+    video_id=extract_video_id(message_with_video.text,messaging_callback=reply_to_callback)
+
+    param_name, param_value, success=extract_parameter(message.text)
+    if not success:
+        bot.send_message(chat_id=message.chat.id,text="🤔 Кажется, это что-то с параметром, но у меня не получается понять что... Сдаюсь")
+        return
+
+    #calling a function for specified parameter name
+    for params in func_map:
+        if param_name in params:
+            func_map[params](video_id=video_id,value=param_value,messaging_callback=write_to_chat_callback)
+            return
+
+    bot.send_message(chat_id=message.chat.id,text=f"Я ещё пока не умею пользоваться {param_name}")
